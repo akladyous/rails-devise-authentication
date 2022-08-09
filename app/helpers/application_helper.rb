097a137ba167dc695ec96c1fd7a5c39c381183dd
Boula Akladyous <akladyous@gmail.com>
2022-08-09 11:49:21 -0400

    added helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+    def feedback_for?(object)
+        return nil if resource.errors.empty?
+        if resource.errors.has_key?(object)
+            return content_tag :div, nil, { class: ['d-block', 'invalid-feedback'] } do
+                resource.errors.full_messages_for(object).to_sentence
+            end
+        end
+        nil
+    end
 end
